be444e09cb8ffb326c21cd868fca709cae830bf9
Tat Nguyen Van <nguyenvantat1182002@gmail.com>
2024-07-17 23:15:16 +0700

    Update project files.

---
+++ app.py
@@ -0,0 +1,175 @@
+import MetaTrader5 as mt5
+import pandas as pd
+import pandas_ta as ta
+import detector
+import threading
+import time
+import numpy as np
+
+from datetime import datetime, timedelta
+
+
+def create_data_frame(symbol: str, timeframe: int) -> pd.DataFrame:
+    rates = mt5.copy_rates_from_pos(symbol, timeframe, 0, 200)
+    
+    df = pd.DataFrame(rates)
+
+    df['time'] = pd.to_datetime(rates['time'], unit='s')
+    df['rsi'] = ta.rsi(df['close'], 14)
+    df['atr'] = ta.atr(df['high'], df['low'], df['close'], 14)
+
+    df.dropna(inplace=True)
+
+    return df
+
+
+risk_amount = 50
+watchlist = {
+    'BTCUSD': {
+        'timeframe': mt5.TIMEFRAME_M5,
+        'unit_factor': 0
+    },
+    'XAUUSD': {
+        'timeframe': mt5.TIMEFRAME_M5,
+        'unit_factor': 100
+    },
+    'EURUSD': {
+        'timeframe': mt5.TIMEFRAME_M5,
+        'unit_factor': 100000
+    },
+    'GBPUSD': {
+        'timeframe': mt5.TIMEFRAME_M5,
+        'unit_factor': 100000
+    },
+    'NZDUSD': {
+        'timeframe': mt5.TIMEFRAME_M5,
+        'unit_factor': 100000
+    },
+    'AUDUSD': {
+        'timeframe': mt5.TIMEFRAME_M5,
+        'unit_factor': 100000
+    },
+}
+
+for symbol in watchlist.keys():
+    watchlist[symbol].update({
+        'next_search_signal_time': datetime.now(),
+        'divergence_time': None,
+        'position': {
+            'price_difference': -1,
+            'take_profit': -1
+        }
+    })
+
+
+def profit_protection_thread():
+    while True:
+        positions = mt5.positions_get()
+        if positions:
+            for position in positions:
+                symbol = position.symbol
+                price_current = position.price_current
+
+                price_difference = watchlist[symbol]['position']['price_difference']
+                take_profit = watchlist[symbol]['position']['take_profit']
+
+                if price_current > take_profit:
+                    watchlist[symbol]['position']['take_profit'] += price_difference
+
+                    request = {
+                        'action': mt5.TRADE_ACTION_SLTP,
+                        'position': position.ticket,
+                        'sl': position.sl + price_difference,
+                    }
+                    mt5.order_send(request)
+
+        time.sleep(1)
+    
+
+def main():
+    while True:
+        for symbol in watchlist:
+            next_search_signal_time = watchlist[symbol]['next_search_signal_time']
+            if datetime.now() > next_search_signal_time:
+                timeframe = watchlist[symbol]['timeframe']
+                time_duration = None
+                match timeframe:
+                    case mt5.TIMEFRAME_M1 | mt5.TIMEFRAME_M5 | mt5.TIMEFRAME_M15:
+                        time_duration = timedelta(minutes=timeframe)
+                    case mt5.TIMEFRAME_H1:
+                        time_duration = timedelta(minutes=60)
+                    case mt5.TIMEFRAME_H4:
+                        time_duration = timedelta(minutes=240)
+                    case _:
+                        print('Chi ho tro 1m, 5m, 15m, 1h, 4h.')
+                        return
+                    
+                df = create_data_frame(symbol, timeframe)
+                result = detector.detect_divergence(df)
+                if result:
+                    divergence_time = result[-1][-1][0]
+
+                    if watchlist[symbol]['divergence_time'] is None:
+                        watchlist[symbol]['divergence_time'] = divergence_time
+
+                    if divergence_time != watchlist[symbol]['divergence_time']:
+                        watchlist[symbol]['divergence_time'] = divergence_time
+
+                        if not mt5.positions_get(symbol=symbol):
+                            info_tick = mt5.symbol_info_tick(symbol)
+                            order_type = None
+                            entry = 0
+                            stop_loss = 0
+                            atr = df['atr'].iloc[-2]
+
+                            signal = result[0][-1]
+                            match signal:
+                                case 0:
+                                    order_type = mt5.ORDER_TYPE_BUY
+                                    entry = info_tick.ask
+                                    stop_loss = entry - atr * 5
+                                case 1:
+                                    order_type = mt5.ORDER_TYPE_SELL
+                                    entry = info_tick.bid
+                                    stop_loss = entry + atr * 5
+
+                            price_difference = abs(entry - stop_loss)
+                            trade_volume = risk_amount / price_difference
+
+                            if watchlist[symbol]['unit_factor'] != 0:
+                                trade_volume = int(trade_volume)
+                                trade_volume = trade_volume / watchlist[symbol]['unit_factor']
+
+                            watchlist[symbol]['position']['price_difference'] = price_difference
+                            watchlist[symbol]['position']['take_profit'] = entry + price_difference
+
+                            trade_volume = round(trade_volume, 2)
+
+                            request = {
+                                'action': mt5.TRADE_ACTION_DEAL,
+                                'symbol': symbol,
+                                'deviation': 10,
+                                'type': order_type,
+                                'volume': trade_volume,
+                                'price': entry,
+                                'sl': stop_loss,
+                            }
+                            print(request)
+
+                            result = mt5.order_send(request)
+                            if not result.retcode == 10009:
+                                print(result)
+                                return
+                        
+                watchlist[symbol]['next_search_signal_time'] = datetime.now() + time_duration
+
+        time.sleep(1)
+
+
+if mt5.initialize():
+    t = threading.Thread(target=profit_protection_thread, daemon=True)
+    t.start()
+    
+    main()
+
+mt5.shutdown()
--- detector.py
+++ detector.py
@@ -3,57 +3,87 @@ import pandas as pd
 from typing import Optional
 
 
-class RSIDivergenceDetector:
-    def __init__(self, symbol: str, timeframe: int):
-        self.symbol = symbol
-        self.timeframe = timeframe
+def get_highest_pivot_bar(df: pd.DataFrame, pivot_candle: pd.Series, window_size: int = 2) -> pd.Series:
+    left_bars = df[df['time'] < pivot_candle['time']].tail(window_size)
+    right_bars = df[df['time'] > pivot_candle['time']].head(window_size)
 
-    def get_highest_pivot_bar(self, df: pd.DataFrame, pivot_bar: pd.Series, window_size: int = 2) -> pd.Series:
-        left_bars = df[df['time'] < pivot_bar['time']].tail(window_size)
-        right_bars = df[df['time'] > pivot_bar['time']].head(window_size)
+    merged_df = pd.concat([left_bars, right_bars])
+    highest_bar = merged_df.nlargest(1, 'high')
+    highest_bar = highest_bar.iloc[-1]
 
-        merged_df = pd.concat([left_bars, right_bars])
-        highest_bar = merged_df.nlargest(1, 'high')
-        highest_bar = highest_bar.iloc[-1]
+    return pivot_candle if highest_bar['high'] < pivot_candle['high'] else highest_bar
 
-        return pivot_bar if highest_bar['high'] < pivot_bar['high'] else highest_bar
 
-    def get_lowest_pivot_bar(self, df: pd.DataFrame, pivot_bar: pd.Series, window_size: int = 2) -> pd.Series:
-        left_bars = df[df['time'] < pivot_bar['time']].tail(window_size)
-        right_bars = df[df['time'] > pivot_bar['time']].head(window_size)
-        
-        merged_df = pd.concat([left_bars, right_bars])
-        lowest_bar = merged_df.nsmallest(1, 'low')
-        lowest_bar = lowest_bar.iloc[-1]
-
-        return pivot_bar if lowest_bar['low'] > pivot_bar['low'] else lowest_bar
+def get_lowest_pivot_bar(df: pd.DataFrame, pivot_candle: pd.Series, window_size: int = 2) -> pd.Series:
+    left_bars = df[df['time'] < pivot_candle['time']].tail(window_size)
+    right_bars = df[df['time'] > pivot_candle['time']].head(window_size)
     
-    def is_bearish_divergence(self, df: pd.DataFrame, current_candle: pd.Series) -> Optional[pd.Series]:
-        nearest_rsi_pivot_high = df[(df['rsi_pivot_high']) & (df['time'] < current_candle['time']) & (df['rsi'] > current_candle['rsi'])]
-            
-        if not nearest_rsi_pivot_high.empty:
-            nearest_rsi_pivot_high = nearest_rsi_pivot_high.iloc[-1]
-            
-            if nearest_rsi_pivot_high['rsi'] > 70:
-                current_candle = self.get_highest_pivot_bar(df, current_candle)
-                nearest_pivot_high_candle = self.get_highest_pivot_bar(df, nearest_rsi_pivot_high)
-                
-                if current_candle['high'] > nearest_pivot_high_candle['high']:
-                    return nearest_rsi_pivot_high
+    merged_df = pd.concat([left_bars, right_bars])
+    lowest_bar = merged_df.nsmallest(1, 'low')
+    lowest_bar = lowest_bar.iloc[-1]
+
+    return pivot_candle if lowest_bar['low'] > pivot_candle['low'] else lowest_bar
+
+
+def is_bearish_divergence(df: pd.DataFrame, current_pivot_high: pd.Series) -> Optional[pd.Series]:
+    nearest_rsi_pivot_high = df[(df['rsi_pivot_high']) & (df['time'] < current_pivot_high['time']) & (df['rsi'] > current_pivot_high['rsi'])]
         
-        return None
-
-    def is_bullish_divergence(self, df: pd.DataFrame, current_candle: pd.Series) -> Optional[pd.Series]:
-        nearest_rsi_pivot_low = df[(df['rsi_pivot_low']) & (df['time'] < current_candle['time']) & (df['rsi'] < current_candle['rsi'])]
-        if not nearest_rsi_pivot_low.empty:
-            nearest_rsi_pivot_low = nearest_rsi_pivot_low.iloc[-1]
-
-            if nearest_rsi_pivot_low['rsi'] < 40:
-                current_candle = self.get_lowest_pivot_bar(df, current_candle)
-                nearest_pivot_low_candle = self.get_lowest_pivot_bar(df, nearest_rsi_pivot_low)
-
-                if current_candle['low'] < nearest_pivot_low_candle['low']:
-                    return nearest_rsi_pivot_low
+    if not nearest_rsi_pivot_high.empty:
+        nearest_rsi_pivot_high = nearest_rsi_pivot_high.iloc[-1]
+        
+        if nearest_rsi_pivot_high['rsi'] > 70:
+            current_pivot_high = get_highest_pivot_bar(df, current_pivot_high)
+            nearest_pivot_high_candle = get_highest_pivot_bar(df, nearest_rsi_pivot_high)
             
-        return None
+            if current_pivot_high['high'] > nearest_pivot_high_candle['high']:
+                return nearest_rsi_pivot_high
     
+    return None
+
+
+def is_bullish_divergence(df: pd.DataFrame, current_pivot_low: pd.Series) -> Optional[pd.Series]:
+    nearest_rsi_pivot_low = df[(df['rsi_pivot_low']) & (df['time'] < current_pivot_low['time']) & (df['rsi'] < current_pivot_low['rsi'])]
+    if not nearest_rsi_pivot_low.empty:
+        nearest_rsi_pivot_low = nearest_rsi_pivot_low.iloc[-1]
+
+        if nearest_rsi_pivot_low['rsi'] < 40:
+            current_pivot_low = get_lowest_pivot_bar(df, current_pivot_low)
+            nearest_pivot_low_candle = get_lowest_pivot_bar(df, nearest_rsi_pivot_low)
+
+            if current_pivot_low['low'] < nearest_pivot_low_candle['low']:
+                return nearest_rsi_pivot_low
+        
+    return None
+
+
+def detect_divergence(df: pd.DataFrame, window_size: int = 3) -> list:
+    df = df.copy()
+
+    df['rsi_pivot_high'] = df['rsi'] == df['rsi'].rolling(2 * window_size + 1, center=True).max()
+    df['rsi_pivot_low'] = df['rsi'] == df['rsi'].rolling(2 * window_size + 1, center=True).min()
+
+    current_pivot_low = df[df['rsi_pivot_low']].iloc[-1]
+    bullish_divergence_point = is_bullish_divergence(df, current_pivot_low)
+    if bullish_divergence_point is not None:
+        tmp = get_lowest_pivot_bar(df, current_pivot_low)
+        nearest_pivot_low_candle = get_lowest_pivot_bar(df, bullish_divergence_point)
+
+        return [
+            [0],
+            [(bullish_divergence_point['time'], bullish_divergence_point['rsi']), (current_pivot_low['time'], current_pivot_low['rsi'])],
+            [(nearest_pivot_low_candle['time'], nearest_pivot_low_candle['low']), (tmp['time'], tmp['low'])]
+        ]
+    
+    current_pivot_high = df[df['rsi_pivot_high']].iloc[-1]
+    bearish_divergence_point = is_bearish_divergence(df, current_pivot_high)
+    if bearish_divergence_point is not None:
+        tmp = get_highest_pivot_bar(df, current_pivot_high)
+        nearest_pivot_high_candle = get_highest_pivot_bar(df, bearish_divergence_point)
+
+        return [
+            [1],
+            [(bearish_divergence_point['time'], bearish_divergence_point['rsi']), (current_pivot_high['time'], current_pivot_high['rsi'])],
+            [(nearest_pivot_high_candle['time'], nearest_pivot_high_candle['high']), (tmp['time'], tmp['high'])]
+        ]
+
+    return []
